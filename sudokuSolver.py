895d4b285adc3e8c5a4abaf7a8b54e9ddf4c166b
JaromMoore <moo25011@byui.edu>
2025-12-07 19:35:20 -0700

    Update sudokuSolver.py

--- sudokuSolver.py
+++ sudokuSolver.py
@@ -80,13 +80,63 @@ def getIndexRow(num):
         return None
 
 def getIndexCol(num):
-    #if num in (0, 9, 18, 27, 36, 45, 54, 63, 72):
+    if num in (0, 9, 18, 27, 36, 45, 54, 63, 72):
         return 0
+    elif num in (1, 10, 19, 28, 37, 46, 55, 64, 73):
+        return 1
+    elif num in (2, 11, 20, 29, 38, 47, 56, 65, 74):
+        return 2
+    elif num in (3, 12, 21, 30, 39, 48, 57, 66, 75):
+        return 3
+    elif num in (4, 13, 22, 31, 40, 49, 58, 67, 76):
+        return 4
+    elif num in (5, 14, 23, 32, 41, 50, 59, 68, 77):
+        return 5
+    elif num in (6, 15, 24, 33, 42, 51, 60, 69, 78):
+        return 6
+    elif num in (7, 16, 25, 34, 43, 52, 61, 70, 79):
+        return 7
+    elif num in (8, 17, 26, 35, 44, 53, 62, 71, 80):
+        return 8
+    else:
+        return None
 
-def fillPossible():
+def getIndexBlock(num):
+    if num in (0, 1, 2, 9, 10, 11, 18, 19, 20):
+        return 0
+    elif num in (3, 4, 6, 12, 13, 14, 21, 22, 23):
+        return 1
+    elif num in (6, 7, 8, 15, 16, 17, 24, 25, 26):
+        return 2
+    elif num in (27, 28, 29, 36, 37, 38, 45, 46, 47):
+        return 3
+    elif num in (30, 31, 32, 39, 40, 41, 48, 49, 50):
+        return 4
+    elif num in (33, 34, 35, 42, 43, 44, 51, 52, 53):
+        return 5
+    elif num in (54, 55, 56, 63, 64, 65, 72, 73, 74):
+        return 6
+    elif num in (57, 58, 59, 66, 67, 68, 75, 76, 77):
+        return 7
+    elif num in (60, 61, 62, 69, 70, 71, 78, 79, 80):
+        return 8
+    else:
+        return None
+    
+def fillPossibleNums():
     for i in range(0,80):
+        temp = []
         if SNumberList[i] == 0:
-            return
+            for j in range(1,9):
+                if j not in getRow(getIndexRow(i)) and j not in getColumn(getIndexCol(i)) and j not in getBlock(getIndexBlock(i)):
+                    temp.append(j)
+        possibleList.append(temp)   
+
+def parsePossibleNums():
+    for i in range(0,80):
+        if len(possibleList[i]) == 1:
+            SNumberList[i] = possibleList[i][1]
+    
 
 
 
@@ -198,7 +248,101 @@ def getBlock(blockNum):
             temp.append(SNumberList[80])
     return temp
         
-
+def getPossibleBlock(blockNum):
+    temp = []
+    match blockNum:
+        case 0:
+            temp.append(possibleList[0])
+            temp.append(possibleList[1])
+            temp.append(possibleList[2])
+            temp.append(possibleList[9])
+            temp.append(possibleList[10])
+            temp.append(possibleList[11])
+            temp.append(possibleList[18])
+            temp.append(possibleList[19])
+            temp.append(possibleList[20])
+        case 1:
+            temp.append(possibleList[3])
+            temp.append(possibleList[4])
+            temp.append(possibleList[5])
+            temp.append(possibleList[12])
+            temp.append(possibleList[13])
+            temp.append(possibleList[14])
+            temp.append(possibleList[21])
+            temp.append(possibleList[22])
+            temp.append(possibleList[23])
+        case 2:
+            temp.append(possibleList[6])
+            temp.append(possibleList[7])
+            temp.append(possibleList[8])
+            temp.append(possibleList[15])
+            temp.append(possibleList[16])
+            temp.append(possibleList[17])
+            temp.append(possibleList[24])
+            temp.append(possibleList[25])
+            temp.append(possibleList[26])
+        case 3:
+            temp.append(possibleList[27])
+            temp.append(possibleList[28])
+            temp.append(possibleList[29])
+            temp.append(possibleList[36])
+            temp.append(possibleList[37])
+            temp.append(possibleList[38])
+            temp.append(possibleList[45])
+            temp.append(possibleList[46])
+            temp.append(possibleList[47])
+        case 4:
+            temp.append(possibleList[30])
+            temp.append(possibleList[31])
+            temp.append(possibleList[32])
+            temp.append(possibleList[39])
+            temp.append(possibleList[40])
+            temp.append(possibleList[41])
+            temp.append(possibleList[48])
+            temp.append(possibleList[49])
+            temp.append(possibleList[50])
+        case 5:
+            temp.append(possibleList[33])
+            temp.append(possibleList[34])
+            temp.append(possibleList[35])
+            temp.append(possibleList[42])
+            temp.append(possibleList[43])
+            temp.append(possibleList[44])
+            temp.append(possibleList[51])
+            temp.append(possibleList[52])
+            temp.append(possibleList[53])
+        case 6:
+            temp.append(possibleList[54])
+            temp.append(possibleList[55])
+            temp.append(possibleList[56])
+            temp.append(possibleList[63])
+            temp.append(possibleList[64])
+            temp.append(possibleList[65])
+            temp.append(possibleList[72])
+            temp.append(possibleList[73])
+            temp.append(possibleList[74])
+        case 7:
+            temp.append(possibleList[57])
+            temp.append(possibleList[58])
+            temp.append(possibleList[59])
+            temp.append(possibleList[66])
+            temp.append(possibleList[67])
+            temp.append(possibleList[68])
+            temp.append(possibleList[75])
+            temp.append(possibleList[76])
+            temp.append(possibleList[77])
+        case 8:
+            temp.append(possibleList[60])
+            temp.append(possibleList[61])
+            temp.append(possibleList[62])
+            temp.append(possibleList[69])
+            temp.append(possibleList[70])
+            temp.append(possibleList[71])
+            temp.append(possibleList[78])
+            temp.append(possibleList[79])
+            temp.append(possibleList[80])
+    return temp
+        
 
 
 
